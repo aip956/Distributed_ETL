33a87d3aed3415f6691848b7e889d56e4c8c8c83
Anthea Ip <antheaip964@gmail.com>
2025-02-27 06:41:12 -0500

    Consumer'

--- stock_consumer.py
+++ stock_consumer.py
@@ -2,6 +2,15 @@ from kafka import KafkaConsumer
 import json
 
 # Kafka Consumer Configuration
-KAFKA_TOPIc = "stock_prices"
+KAFKA_TOPIC = "stock_prices"
 KAFKA_BROKER = "localhost:9092"
 
+consumer = KafkaConsumer(
+    KAFKA_TOPIC,
+    bootstrap_servers = KAFKA_BROKER,
+    value_deserializer = lambda v: json.loads(v.decode("utf-8")),
+    auto_offset_resset = "earliest", # Start from the beginning of topic
+    enable_auto_commit = True # Auto mark messages as read
+)
+
+# Consume messages
