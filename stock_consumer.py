c53f9dd8da22ad0ec204f6b6555f93d2529b9f69
Anthea Ip <antheaip964@gmail.com>
2025-02-27 22:09:11 -0500

    Consumer working

--- stock_consumer.py
+++ stock_consumer.py
@@ -9,7 +9,7 @@ consumer = KafkaConsumer(
     KAFKA_TOPIC,
     bootstrap_servers = KAFKA_BROKER,
     value_deserializer = lambda v: json.loads(v.decode("utf-8")),
-    auto_offset_resset = "earliest", # Start from the beginning of topic
+    auto_offset_reset = "earliest", # Start from the beginning of topic
     enable_auto_commit = True # Auto mark messages as read
 )
 
